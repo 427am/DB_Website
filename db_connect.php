d77437467dd4890f000ff6bce78ceec893b2f6c8
Sofia Bernal <sb22i@fsu.edu>
2025-11-28 14:30:25 -0500

    upating db_connect file

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+//SOFIA
+
+
+<?php
+$connection = new mysqli(
+    "127.0.0.1",
+    "root",
+    "root",
+    "realestate",
+    8889
+);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+?>
--- realestate_site/db_connect.php
+++ realestate_site/db_connect.php
@@ -1,11 +1,8 @@
+//MADDY AND ISABELLA
+
 <?php
-$connection = new mysqli(
-    "127.0.0.1",
-    "root",
-    "root",
-    "realestate",
-    8889
-);
+
+$connection = new mysqli("127.0.0.1", "root", "", "realestate", 3306);
 
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
