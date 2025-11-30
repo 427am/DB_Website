59d9f3aca883f8c5044b1b9b79eb26f4aea7d160
427am <maddymountsinai@gmail.com>
2025-11-30 17:28:30 -0500

    Added Website Images

--- realestate_site/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$connection = new mysqli("127.0.0.1", "root", "", "realestate", 3306);
-
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
-}
-?>
